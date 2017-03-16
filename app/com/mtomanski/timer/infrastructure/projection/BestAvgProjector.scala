dd33d03afe48c8c568c9e4aaa78c928633b1c60f
mtomanski <mtomanski@nexelem.com>
2017-03-16 09:46:35 +0100

    Fix projection

--- app/com/mtomanski/timer/infrastructure/projection/BestAvgProjector.scala
+++ app/com/mtomanski/timer/infrastructure/projection/BestAvgProjector.scala
@@ -2,16 +2,20 @@ package com.mtomanski.timer.infrastructure.projection
 
 import javax.inject.Inject
 
+import akka.Done
 import akka.actor.Props
 import akka.persistence.cassandra.query.scaladsl.CassandraReadJournal
 import akka.persistence.query.{EventEnvelope, PersistenceQuery}
 import akka.persistence.{PersistentActor, RecoveryCompleted}
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.Sink
 import com.mtomanski.timer.domain.model.BestAvg
 import com.mtomanski.timer.domain.model.Speedcuber.BestAvgChanged
 import com.mtomanski.timer.domain.repository.BestAvgRepository
 import org.slf4j.LoggerFactory
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 class BestAvgProjector @Inject()(repo: BestAvgRepository) extends PersistentActor {
   private val logger = LoggerFactory.getLogger(getClass)
@@ -20,9 +24,6 @@ class BestAvgProjector @Inject()(repo: BestAvgRepository) extends PersistentActo
   var state = State(offset = 0, firstOffsetSaved = false)
 
   override def receiveCommand: Receive = {
-    case event: BestAvgChanged =>
-      logger.info(s"Updating view of best averages with $event")
-      repo.upsert(BestAvg(event.user, event.millis))
     case SaveOffset(value) => persist(OffsetSaved(value)) { offsetSaved =>
       persistOffset(offsetSaved.offset)
     }
@@ -40,8 +41,14 @@ class BestAvgProjector @Inject()(repo: BestAvgRepository) extends PersistentActo
 
   private def startProcessing() = {
     val source = readJournal.eventsByTag("Speedcuber", state.offset).drop(dropIfNeeded)
-    source.map{enveloped => self ! enveloped.event; enveloped}
-      .runWith(Sink.foreach{ enveloped => self ! SaveOffset(enveloped.offset)})
+    source.mapAsync(1) {
+      case EventEnvelope(offset, _, _, event: BestAvgChanged) =>
+        logger.info(s"Updating view of best averages with $event")
+        repo.upsert(BestAvg(event.user, event.millis)).map(_ => offset)
+      case EventEnvelope(offset, _, _, event) =>
+        logger.debug(s"Ignoring $event during projection")
+        Future.successful(offset)
+    }.runWith(Sink.foreach(offset => self ! SaveOffset(offset)))
     logger.info("Event stream processing started")
   }
 
