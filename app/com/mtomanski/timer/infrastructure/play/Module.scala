5dee590797178912540aece8f49dad00db4340d2
mtomanski <mtomanski@nexelem.com>
2017-03-22 22:26:16 +0100

    singleton

--- app/com/mtomanski/timer/infrastructure/play/Module.scala
+++ app/com/mtomanski/timer/infrastructure/play/Module.scala
@@ -2,19 +2,30 @@ package com.mtomanski.timer.infrastructure.play
 
 import javax.inject.{Inject, Provider}
 
-import akka.actor.{ActorRef, ActorSystem}
+import akka.actor.{ActorRef, ActorSystem, PoisonPill}
+import akka.cluster.singleton.{
+  ClusterSingletonManager,
+  ClusterSingletonManagerSettings,
+  ClusterSingletonProxy,
+  ClusterSingletonProxySettings
+}
 import com.google.inject.AbstractModule
 import com.google.inject.name.Names
 import com.mtomanski.timer.domain.repository.BestAvgRepository
 import com.mtomanski.timer.domain.service.SpeedcuberLocator
 import com.mtomanski.timer.infrastructure.locator.SpeedcuberClusterShardLocator
 import com.mtomanski.timer.infrastructure.projection.BestAvgProjector
-import com.mtomanski.timer.infrastructure.projection.builder.{BestAvgViewBuilder, PostgresBestAvgViewBuilder}
+import com.mtomanski.timer.infrastructure.projection.builder.{
+  BestAvgViewBuilder,
+  PostgresBestAvgViewBuilder
+}
 import com.mtomanski.timer.infrastructure.repository.PostgresBestAvgRepository
 import play.api.libs.concurrent.AkkaGuiceSupport
 import play.api.{Configuration, Environment}
 
-class Module(environment: Environment, configuration: Configuration) extends AbstractModule with AkkaGuiceSupport {
+class Module(environment: Environment, configuration: Configuration)
+    extends AbstractModule
+    with AkkaGuiceSupport {
   override def configure(): Unit = {
     bind(classOf[ActorRef])
       .annotatedWith(Names.named("view-projector"))
@@ -26,8 +37,22 @@ class Module(environment: Environment, configuration: Configuration) extends Abs
   }
 }
 
-class ProjectorProvider @Inject()(actorSystem: ActorSystem, viewBuilder: BestAvgViewBuilder) extends Provider[ActorRef] {
-  private lazy val instance = actorSystem.actorOf(BestAvgProjector.props(viewBuilder))
+class ProjectorProvider @Inject()(actorSystem: ActorSystem,
+                                  viewBuilder: BestAvgViewBuilder)
+    extends Provider[ActorRef] {
+
+  val clustrerSingletonManager = actorSystem.actorOf(
+    ClusterSingletonManager.props(
+      singletonProps = BestAvgProjector.props(viewBuilder),
+      terminationMessage = PoisonPill,
+      settings = ClusterSingletonManagerSettings(actorSystem)), "proj")
+
+  val proxy = ClusterSingletonProxy.props(
+    "/user/consumer",
+    ClusterSingletonProxySettings(actorSystem))
+
+  private lazy val instance =
+    actorSystem.actorOf(proxy)
 
   override def get(): ActorRef = instance
 }
