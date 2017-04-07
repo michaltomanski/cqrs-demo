7e0d75c983062da198c84e0a48322eab95c6b8fd
mtomanski <mtomanski@nexelem.com>
2017-04-07 11:18:55 +0300

    Log command

--- app/com/mtomanski/timer/api/SpeedcuberCommandApi.scala
+++ app/com/mtomanski/timer/api/SpeedcuberCommandApi.scala
@@ -6,6 +6,7 @@ import com.mtomanski.timer.domain.model.Speedcuber.{AddTime, TimeAdded}
 import akka.pattern.ask
 import akka.util.Timeout
 import com.mtomanski.timer.domain.service.SpeedcuberLocator
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
@@ -13,8 +14,11 @@ import scala.concurrent.duration._
 class SpeedcuberCommandApi @Inject()(speedcuberLocator: SpeedcuberLocator) {
 
   implicit val timeout: Timeout = 3.seconds
+  private val logger = LoggerFactory.getLogger(getClass)
 
-  def addTime(command: AddTime): Future[TimeAdded] =
+  def addTime(command: AddTime): Future[TimeAdded] = {
+    logger.info(s"Handling $command command")
     (speedcuberLocator.speedcuber() ? command).mapTo[TimeAdded]
+  }
 
 }
